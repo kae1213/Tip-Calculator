79a2c3aaeefd1e03a99ea6c60bbea16f71249c4d
Kelvin Martinez <klvnmrtnz123456789@gmail.com>
2020-07-21 20:26:21 -0700

    Initial Commit

---
+++ preWork2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  preWork2020
+//
+//  Created by Kelvin Martinez on 7/21/20.
+//  Copyright © 2020 Kelvin Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ preWork2020Tests/preWork2020Tests.swift
@@ -0,0 +1,34 @@
+//
+//  preWork2020Tests.swift
+//  preWork2020Tests
+//
+//  Created by Kelvin Martinez on 7/21/20.
+//  Copyright © 2020 Kelvin Martinez. All rights reserved.
+//
+
+import XCTest
+@testable import preWork2020
+
+class preWork2020Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
